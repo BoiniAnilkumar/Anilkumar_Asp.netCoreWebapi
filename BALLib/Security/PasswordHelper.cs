b223feb3c3862b4dcae9cf04e26426e0e56bd3ab
BoiniAnilkumar <itsanilkumar1998@gmail.com>
2026-03-17 18:15:08 +0530

    Employee

---
+++ BALLib/Security/PasswordHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BCrypt.Net;
+namespace BALLib.Security
+{
+    public static class PasswordHelper
+    {
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+        public static bool verifyPassword(string enteredpassword,string storedHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(enteredpassword, storedHash);
+        }
+    }
+}
